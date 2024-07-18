5f66d21e5fdfed9e0ff33c234d255a5ffe8d748e
OdedHorowits <Horowits@gmail.com>
2024-07-18 23:16:27 +0300

    more configurations

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Password:
 
 $ docker pull nvcr.io/nvidia/isaac-sim:4.0.0
 
+Maybe need to install 'apt install ros-humble-vision-msgs'
+If it is not found, follow https://stackoverflow.com/questions/72591758/unable-to-locate-ros-packages
+
 
 ## Running Python Code from External Editors
 * https://docs.omniverse.nvidia.com/isaacsim/latest/advanced_tutorials/tutorial_advanced_code_editors.html
--- scripts/run_docker_isaac_sim.sh
+++ scripts/run_docker_isaac_sim.sh
@@ -42,6 +42,7 @@ run_docker_cmd+="  --rm \
     -v ./isaac_sim/logs:/root/.nvidia-omniverse/logs:rw \
     -v ./isaac_sim/data:/root/.local/share/ov/data:rw \
     -v ./isaac_sim/documents:/root/Documents:rw \
+    -v $(dirname -- "$( readlink -f -- "$0"; )")/../scripts/bashrc:/root/.bashrc:ro \
     -v $(dirname -- "$( readlink -f -- "$0"; )")/..:/workspace"
 
 run_docker_cmd+=" $docker_image"
