27a83f6ce83140ef450f009ac8cea4aa43dbad1b
daphnaa <daphna.ashkenazi@gmail.com>
2026-01-13 15:02:21 +0200

    update the task to select model and costmap for mapping create_map_from_video.py is the main to get map

--- sparx_agency/tasks/create_map_from_video.py
+++ sparx_agency/tasks/create_map_from_video.py
@@ -1,5 +1,8 @@
 import rclpy
-from sparx_agency.core.mapping.pipeline.mapping_pipeline import MappingPipeline
+
+from sparx_agency.core.mapping.depth import DepthAnythingV2DepthModel
+from sparx_agency.core.mapping.depth.depth_anything_v2 import DepthAnythingV2Config
+from sparx_agency.core.mapping.pipeline.mapping_pipeline import MappingPipeline, PinholeCloudGenerator
 from sparx_agency.core.mapping.costmap.probabilistic_grid import ProbabilisticGridCostmap
 from sparx_agency.core.mapping.costmap.probabilistic_grid_config import ProbabilisticGridConfig
 from sparx_agency.robots.SJTU.adapters.gazebo_ros2_ingest import GazeboRos2Ingestor
@@ -8,7 +11,23 @@ from sparx_agency.robots.SJTU.adapters.gazebo_ros2_ingest import GazeboRos2Inges
 def main():
     rclpy.init()
 
-    node = GazeboRos2Ingestor()
+    # --- Step 1: Configure the Brain ---
+    map_cfg = ProbabilisticGridConfig(points_to_occupied=30)
+    costmap = ProbabilisticGridCostmap(map_cfg)
+
+    depth_model = DepthAnythingV2DepthModel(DepthAnythingV2Config())  # vits
+    cloud_gen = PinholeCloudGenerator()
+
+    # --- Step 2: Assemble the Pipeline ---
+    pipeline = MappingPipeline(
+        costmap=costmap,
+        depth_model=depth_model,
+        cloud_generator=cloud_gen
+    )
+
+    # --- Step 3: Plug into the Drone Adapter ---
+    # This node handles the Gazebo/ROS 'plumbing'
+    node = GazeboRos2Ingestor(pipeline=pipeline, costmap=costmap)
 
     try:
         print("Task Started: Mapping environment from video stream...")
