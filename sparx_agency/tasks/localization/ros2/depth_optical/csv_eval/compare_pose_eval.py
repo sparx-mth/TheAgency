806f5c4b80fe4460d05b10acd692c2a20e5e2475
Shir Ben Ami <shirbena@post.bgu.ac.il>
2026-01-26 17:28:48 +0200

    add the images of the results

--- sparx_agency/tasks/localization/ros2/depth_optical/csv_eval/compare_pose_eval.py
+++ sparx_agency/tasks/localization/ros2/depth_optical/csv_eval/compare_pose_eval.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# this script compares two pose evaluation CSV files (from pose_eval_rosbag2.py)
 """
 run the script:
 python3 compare_pose_eval.py \
